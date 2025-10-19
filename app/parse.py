e1c279d23cb6d438d3f31494f9e8a224c99c44b6
Otstoy <otstoy.sxe@gmail.com>
2025-10-19 22:23:48 +0300

    feat: implement quotes parser

--- app/parse.py
+++ app/parse.py
@@ -1,4 +1,13 @@
+import csv
 from dataclasses import dataclass
+from typing import Any, Generator
+
+import requests
+from urllib.parse import urljoin
+
+from bs4 import BeautifulSoup
+
+BASE_URL = "https://quotes.toscrape.com/"
 
 
 @dataclass
@@ -8,8 +17,48 @@ class Quote:
     tags: list[str]
 
 
+def get_pages() -> Generator[str, Any, None]:
+    page_number = 0
+    while True:
+        page_number += 1
+        page_url = urljoin(BASE_URL, f"page/{page_number}/")
+        response = requests.get(page_url)
+        soup = BeautifulSoup(response.text, "html.parser")
+
+        if not soup.select(".quote"):
+            break
+
+        yield page_url
+
+
+def parse_page(page_url: str) -> list[Quote]:
+    text = requests.get(page_url).content
+    soup = BeautifulSoup(text, "html.parser")
+    quotes = soup.select(".quote")
+    return [Quote(
+        text=quote.select_one(".text").text,
+        author=quote.select_one(".author").text,
+        tags=[tag.text for tag in quote.select(".tag")]
+    ) for quote in quotes]
+
+
+def parse_pages() -> list[Quote]:
+    quotes = []
+    for page_url in get_pages():
+        quotes.extend(parse_page(page_url))
+    return quotes
+
+
 def main(output_csv_path: str) -> None:
-    pass
+    quotes = parse_pages()
+
+    with open(output_csv_path, "w", newline="", encoding="utf-8") as f:
+        writer = csv.writer(f)
+        writer.writerow(["text", "author", "tags"])
+        for quote in quotes:
+            writer.writerow([
+                quote.text, quote.author, quote.tags
+            ])
 
 
 if __name__ == "__main__":
